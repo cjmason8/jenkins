25e7787d65c20ff173cd761be6c5bd495af05c92
Chris Mason <cjmason8@gmail.com>
2019-03-10 16:01:14 +1100

    add jenkins stack

--- deploy.sh
+++ deploy.sh
@@ -6,15 +6,15 @@ RANCHER_URL=http://80.241.221.122:8080/v2-beta/projects/1a5
 BASE_DIR=${PWD}
 
 echo "Force pulling..."
-rancher-compose --url ${RANCHER_URL} --access-key ${RANCHER_ACCESS_KEY} --secret-key ${RANCHER_SECRET_KEY} -p postgres -f ${BASE_DIR}/docker-compose.yml pull
+rancher-compose --url ${RANCHER_URL} --access-key ${RANCHER_ACCESS_KEY} --secret-key ${RANCHER_SECRET_KEY} -p jenkins -f ${BASE_DIR}/docker-compose.yml pull
 
 echo "Starting deployment..."
-rancher-compose --url ${RANCHER_URL} --access-key ${RANCHER_ACCESS_KEY} --secret-key ${RANCHER_SECRET_KEY} -r ${BASE_DIR}/rancher-compose..yml -p postgres -f ${BASE_DIR}/docker-compose.yml up --upgrade -d --pull --batch-size 1
+rancher-compose --url ${RANCHER_URL} --access-key ${RANCHER_ACCESS_KEY} --secret-key ${RANCHER_SECRET_KEY} -r ${BASE_DIR}/rancher-compose..yml -p jenkins -f ${BASE_DIR}/docker-compose.yml up --upgrade -d --pull --batch-size 1
 
 if [ $? -eq 0 ]; then
   echo "Deploy success! Confirming..."
-  rancher-compose --url ${RANCHER_URL} --access-key ${RANCHER_ACCESS_KEY} --secret-key ${RANCHER_SECRET_KEY} -p postgres -f ${BASE_DIR}/docker-compose.yml up --confirm-upgrade -d --batch-size 1
+  rancher-compose --url ${RANCHER_URL} --access-key ${RANCHER_ACCESS_KEY} --secret-key ${RANCHER_SECRET_KEY} -p jenkins -f ${BASE_DIR}/docker-compose.yml up --confirm-upgrade -d --batch-size 1
 else
   echo "Deploy failed :( rolling back..."
-  rancher-compose --url ${RANCHER_URL} --access-key ${RANCHER_ACCESS_KEY} --secret-key ${RANCHER_SECRET_KEY} -p postgres -f ${BASE_DIR}/docker-compose.yml up --rollback -d --batch-size 1
+  rancher-compose --url ${RANCHER_URL} --access-key ${RANCHER_ACCESS_KEY} --secret-key ${RANCHER_SECRET_KEY} -p jenkins -f ${BASE_DIR}/docker-compose.yml up --rollback -d --batch-size 1
 fi
