2d66f7bf873e72aff121641c4e7e6b9c525f8905
Chris Mason <cjmason8@gmail.com>
2019-04-25 10:14:17 +1000

    merge

--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-<<<<<<< HEAD
 set -e
 
 FULL_IMAGE_NAME="jenkins"
@@ -14,9 +13,3 @@ docker tag ${FULL_IMAGE_NAME}:${TAG_NAME} cjmason8/${FULL_IMAGE_NAME}:${TAG_NAME
 docker tag ${FULL_IMAGE_NAME}:${TAG_NAME} cjmason8/${FULL_IMAGE_NAME}:latest
 docker push cjmason8/${FULL_IMAGE_NAME}:latest
 docker push cjmason8/${FULL_IMAGE_NAME}:${TAG_NAME}
-=======
-VERSION=$1
-
-docker build --no-cache --pull -t cjmason8/jenkins:$VERSION .
-docker push cjmason8/jenkins:$VERSION
->>>>>>> b8e3b1ce3686074e6baa5db4b4ef3c8cb49e2290
